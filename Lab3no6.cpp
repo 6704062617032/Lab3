1085cbfd427b93d4f483d89a9f6d5dd1e3c82d8e
6704062617032 แผ่นดิน แก้วบรรจง <s6704062617032@email.kmutnb.ac.th>
2026-01-05 11:29:27 +0700

    Lab3no6

---
+++ Lab3no6.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main() {
+	int score;
+	scanf("%d", &score);
+	if(score >= 68) {
+		if(score >= 87) {
+			printf("A");
+		}
+		else if (score >= 79) {
+			printf("B");
+		}
+		else if (score >= 71) {
+			printf("C");
+		}
+	} else {
+		if(score < 68) {
+			if (score >= 63)
+			printf("D");
+		}
+		else{
+			printf("F");
+		}
+	}
+	
+	
+	
+	return 0;
+}
