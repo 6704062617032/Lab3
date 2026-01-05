36d562bcee718e286946f20d84634cac7ee51369
6704062617032 แผ่นดิน แก้วบรรจง <s6704062617032@email.kmutnb.ac.th>
2026-01-05 12:14:21 +0700

    Lab3no9

---
+++ Lab3no9.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+	int mouth;
+	
+	scanf("%d",&mouth);
+	
+	switch(mouth) {
+		case 1:
+			printf("winter"); break;
+		case 2:
+			printf("winter"); break;
+		case 3:
+			printf("summer"); break;
+		case 4:
+			printf("summer"); break;
+		case 5:
+			printf("summer"); break;
+		case 6:
+			printf("rainy"); break;
+		case 7:
+			printf("rainy"); break;
+		case 8:
+			printf("rainy"); break;
+		case 9:
+			printf("rainy"); break;
+		case 10:
+			printf("rainy"); break;
+		case 11:
+			printf("winter"); break;
+		case 12:
+			printf("winter"); break;
+		default:
+			printf("Invalid");
+	
+	return 0;
+			
+	}
+}
